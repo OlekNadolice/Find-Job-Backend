c36d85d3b7e00808f90f7df620c85b3d7f5b7b24
OlekNadolice <nadolice1997@wp.pl>
2023-09-17 19:57:52 +0200

    Add enums to common module

---
+++ common/src/main/java/org/example/enums/CompanyCategoryType.java
@@ -0,0 +1,14 @@
+package org.example.enums;
+
+public enum CompanyCategoryType {
+    TECHNOLOGY,
+    HEALTHCARE,
+    FINANCE,
+    RETAIL,
+    MANUFACTURING,
+    HOSPITALITY,
+    EDUCATION,
+    ENTERTAINMENT,
+    TRANSPORTATION,
+    CONSULTING,
+}
