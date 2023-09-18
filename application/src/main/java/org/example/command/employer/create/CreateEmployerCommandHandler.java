ff3958f36f6f7f999682406a030542b5f3deb8a9
OlekNadolice <nadolice1997@wp.pl>
2023-09-18 11:26:59 +0200

    Add registration of employer

---
+++ application/src/main/java/org/example/command/employer/create/CreateEmployerCommandHandler.java
@@ -0,0 +1,2 @@
+package org.example.command.employer.create;public class CreateEmployerCommandHandler {
+}
---
+++ infrastructure/src/main/java/org/example/security/authentication/RegisterUserDTO.java
@@ -0,0 +1,2 @@
+package org.example.security.authentication;public class RegisterUserDTO {
+}
