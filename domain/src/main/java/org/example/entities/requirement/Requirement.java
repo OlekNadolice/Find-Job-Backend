c5ee3fec221dfbad90636db3a3aeeff7abde636a
OlekNadolice <nadolice1997@wp.pl>
2023-09-28 07:56:11 +0200

    JOB-10/CREATE-ENTITIES-FOR-BENEFIT-AND-REQUIREMENT

---
+++ domain/src/main/java/org/example/entities/requirement/Requirement.java
@@ -0,0 +1,42 @@
+package org.example.entities.requirement;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.entities.advertisement.Advertisement;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Requirement {
+
+    @Id
+    private UUID id;
+
+
+    private  String description;
+
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name = "advertisement_id")
+    private Advertisement advertisement;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Requirement that = (Requirement) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ infrastructure/src/main/java/org/example/repositories/requirement/RequirementCommandRepository.java
@@ -0,0 +1,11 @@
+package org.example.repositories.requirement;
+
+import org.example.entities.requirement.Requirement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RequirementCommandRepository extends JpaRepository<Requirement, UUID> {
+}
